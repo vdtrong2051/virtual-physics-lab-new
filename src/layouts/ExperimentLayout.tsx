56f20883ec6746f3d7d3fbf149cff74eb0c27db9
unknown <voductrong205051@gmail.com>
2026-09-19 14:40:10 +0700

    refactor(styles): extract shared experiment styles

--- src/layouts/ExperimentLayout.tsx
+++ src/layouts/ExperimentLayout.tsx
@@ -14,6 +14,7 @@ import type {
   ExperimentDomain,
 } from "../experiments/registry";
 
+import "../styles/experiment/shared/index.css";
 import "../styles/experiment.css";
 
 const domainAccentClasses:
